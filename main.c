e08aa08b2da000486a71f604c1d2df18be41086c
Sahdevagarwal <Sahdevagarwal1@gmail.com>
2026-02-23 19:02:11 +0530

    Added Player Turns

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 
 void Instructions(){
     printf("\t\t\t Tic-Tac-Toe\n\n");
-    printf("Choose a cell numbered from 1 to 9 as below "
-           "and play\n\n");
+    printf("Choose a cell numbered from 1 to 9 as below and play\n\n");
 
     printf("\t\t\t 1 | 2 | 3 \n");
     printf("\t\t\t------------\n");
@@ -15,23 +14,11 @@ void Instructions(){
 }
 
 void ShowBoard(char board[3][3]){
-    printf("\n");
-
-    for(int i=0;i<3;i++){
-        printf("\t\t\t ");
-        for(int j=0;j<3;j++){
-            printf(" %c ", board[i][j]);
-            if(j<2){
-                printf("|");
-            }
-        }
-        printf("\n");
-
-        if(i<2)
-            printf("\t\t\t-------------\n");
-    }
-
-    printf("\n");
+    printf("\t\t\t %c | %c | %c \n",board[0][0],board[0][1],board[0][2]);
+    printf("\t\t\t------------\n");
+    printf("\t\t\t %c | %c | %c \n",board[1][0],board[1][1],board[1][2]);
+    printf("\t\t\t------------\n");
+    printf("\t\t\t %c | %c | %c \n",board[2][0],board[2][1],board[2][2]);
 }
 
 int main(){
@@ -43,4 +30,26 @@ int main(){
     }
     Instructions();
     ShowBoard(board);
+    
+    char player = 'X';
+    int ch;
+
+    while(1){
+        printf("Player %c , Enter position (1-9): ",player);
+        scanf("%d",&ch);
+
+        int row = (ch-1)/3;
+        int col = (ch-1)%3;
+
+        if(board[row][col] == ' '){
+            board[row][col] = player;
+        }
+        else{
+            printf("Cell is Not Empty!\n");
+            continue;
+        }
+        ShowBoard(board);
+        player = (player == 'X') ? 'O' : 'X';
+    }
+    
 }
