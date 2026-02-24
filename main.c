55b70ec1a8fe82a3896d6b392a11b69a26d9a291
Sahdevagarwal <Sahdevagarwal1@gmail.com>
2026-02-24 09:48:36 +0530

    Added Easy Bot

--- main.c
+++ main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char player = 'X';
+int moves = 9;
+typedef void (*BotMove)(char board[3][3]);
 
 void Instructions(){
     printf("\t\t\t Tic-Tac-Toe\n\n");
@@ -21,7 +27,7 @@ void ShowBoard(char board[3][3]){
     printf("\t\t\t %c | %c | %c \n",board[2][0],board[2][1],board[2][2]);
 }
 
-int WinCheck(char board[3][3],char player){
+int WinCheck(char board[3][3]){
     for(int i = 0;i < 3;i++){
         if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' '){
             printf("Player %c has won!\n",player);
@@ -45,19 +51,10 @@ int WinCheck(char board[3][3],char player){
     }
 }
 
-int main(){
-    char board[3][3];
-    for(int i =0;i<3;i++){
-        for(int j=0;j<3;j++){
-            board[i][j] = ' ';
-        }
-    }
+void Player(char board[3][3]){
+    int ch;
     Instructions();
     ShowBoard(board);
-    
-    char player = 'X';
-    int ch,moves=9;
-
     while(1){
         printf("Player %c , Enter position (1-9): ",player);
         scanf("%d",&ch);
@@ -74,7 +71,7 @@ int main(){
         }
         moves--;
         ShowBoard(board);
-        if(WinCheck(board,player)){
+        if(WinCheck(board)){
             break;
         }
         else if(moves == 0){
@@ -83,5 +80,101 @@ int main(){
         }
         player = (player == 'X') ? 'O' : 'X';
     }
-    
+}
+
+void EasyBot(char board[3][3]){
+    int ch,row,col;
+    srand(time(NULL));
+
+    while(1){
+        ch = rand()% 9 + 1;
+        row = (ch-1)/3;
+        col = (ch-1)%3;
+
+        if (board[row][col] == ' '){
+            board[row][col] = 'O';
+        }
+        else{
+            continue;
+        }
+        break;
+    }
+}
+
+void Gameloop(char board[3][3], BotMove bot){
+    int ch;
+    Instructions();
+    ShowBoard(board);
+    while (1){
+        if (player == 'X'){
+            printf("Player %c , Enter position (1-9): ",player);
+            scanf("%d",&ch);
+
+            int row = (ch-1)/3;
+            int col = (ch-1)%3;
+
+            if(board[row][col] == ' '){
+                board[row][col] = player;
+            }
+            else{
+                printf("Cell is Not Empty!\n");
+                continue;
+            }
+        }
+        else{
+            bot(board);
+        }
+        moves--;
+        ShowBoard(board);
+        printf("-\t-\t-\t-\t-\t-\t-\t-\n\n");
+        if(WinCheck(board)){
+            break;
+        }
+        else if(moves == 0){
+            printf("Draw!");
+            break;
+        }
+        player = (player == 'X') ? 'O' : 'X';
+    }
+}
+
+int main(){
+    char board[3][3];
+    for(int i =0;i<3;i++){
+        for(int j=0;j<3;j++){
+            board[i][j] = ' ';
+        }
+    }
+    int ch;
+    printf("Enter 1 to play against another or 2 to play against computer: ");
+    scanf("%d",&ch);
+    if (ch == 1){
+        Player(board);
+    }
+    else if (ch == 2){
+        int n;
+        printf("\n---------Choose Difficulty---------\n");
+        printf("1.Easy\n");
+        printf("2.Medium\n");
+        printf("3.Hard\n");
+        printf("Enter Your Choice: ");
+        scanf("%d",&n);
+        switch (n){
+            case 1:
+                Gameloop(board,EasyBot);
+                break;
+            case 2:
+                printf("Coming Soon");
+                break;
+            case 3:
+                printf("Coming Soon");
+                break;
+            default:
+                printf("Invalid Choice!");
+                break;
+        }
+    }
+    else{
+        printf("Invalid Input!");
+    }
 }
